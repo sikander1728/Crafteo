9d123624f97ebd1240011114f89ee5c0cdcc18cf
sikander1728 <sikandersultan15@gmail.com>
2023-03-20 19:18:47 +0500

    first commit

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { connectDatabase } = require('./dbconfig')
+const router = require('./routers/user-routes')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use('/api', router)
+
+//Database call
+connectDatabase()
+ 
+app.get('/', (req, res)=>{
+    res.json({message: "Request Arrived"})
+})
+
+//Server PORT
+const port = process.env.PORT || 5000
+
+app.listen(port, (req, res)=>{
+    console.log('server is running on port', port)
+})
